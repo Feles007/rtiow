dbe52f59956c82cba50b1748ed969fc84b0f9eda
Feles007 <75643325+Feles007@users.noreply.github.com>
2025-08-09 09:07:16 -0700

    Move Sphere Hittable impl

--- src/sphere.rs
+++ src/sphere.rs
@@ -1,4 +1,8 @@
+use crate::hit_record::HitRecord;
+use crate::hittable::Hittable;
+use crate::interval::Interval;
 use crate::material::MaterialReference;
+use crate::ray::Ray;
 use crate::utils::Point3;
 
 #[derive(Debug, Copy, Clone)]
@@ -15,5 +19,33 @@ impl Sphere {
 			radius,
 			material,
 		}
+	}
+}
+impl Hittable for Sphere {
+	fn hit(&self, ray: Ray, interval: Interval) -> Option<HitRecord> {
+		let oc = self.center - ray.origin();
+		let a = ray.direction().magnitude_squared();
+		let h = ray.direction().dot(&oc);
+		let c = oc.magnitude_squared() - self.radius * self.radius;
+
+		let discriminant = h * h - a * c;
+		if discriminant < 0.0 {
+			return None;
+		}
+
+		let sqrtd = discriminant.sqrt();
+
+		let mut root = (h - sqrtd) / a;
+		if root <= interval.min || interval.max <= root {
+			root = (h + sqrtd) / a;
+			if root <= interval.min || interval.max <= root {
+				return None;
+			}
+		}
+
+		let point = ray.at(root);
+		let normal = (point - self.center) / self.radius;
+
+		Some(HitRecord::new(ray, normal, point, root, self.material))
 	}
 }
