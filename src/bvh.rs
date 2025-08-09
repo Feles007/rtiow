7cf19939bc1b3ae1c240fe95164e2f3aa17d4e93
Feles007 <75643325+Feles007@users.noreply.github.com>
2025-08-09 09:23:24 -0700

    AABB creation

--- src/bvh.rs
+++ src/bvh.rs
@@ -1,5 +1,7 @@
 use crate::material::Material;
+use crate::sphere::Sphere;
 use crate::world::World;
+use glm::Vec3;
 
 pub struct BvhWorld {
 	materials: Vec<Material>,
@@ -8,5 +10,30 @@ impl BvhWorld {
 	pub fn new(world: World) -> Self {
 		let (spheres, materials) = world.decompose();
 		Self { materials }
+	}
+}
+struct Aabb {
+	min: Vec3,
+	max: Vec3,
+}
+impl Aabb {
+	pub fn from_spheres(spheres: &[Sphere]) -> Self {
+		let mut min = Vec3::zeros();
+		let mut max = Vec3::zeros();
+
+		for sphere in spheres {
+			let limit_max = sphere.center + Vec3::from_element(sphere.radius);
+			let limit_min = sphere.center - Vec3::from_element(sphere.radius);
+
+			min.x = min.x.min(limit_min.x);
+			min.y = min.y.min(limit_min.y);
+			min.z = min.z.min(limit_min.z);
+
+			max.x = max.x.max(limit_max.x);
+			max.y = max.y.max(limit_max.y);
+			max.z = max.z.max(limit_max.z);
+		}
+
+		Self { min, max }
 	}
 }
