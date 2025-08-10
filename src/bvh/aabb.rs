d1ecf95d272124536e9e00ca874ae5535e4d81c9
Feles007 <75643325+Feles007@users.noreply.github.com>
2025-08-10 14:07:19 -0700

    Remove #[inline(never)]

--- src/bvh/aabb.rs
+++ src/bvh/aabb.rs
@@ -8,7 +8,6 @@ pub struct Aabb {
 }
 impl Aabb {
 	// https://tavianator.com/2015/ray_box_nan.html
-	#[inline(never)]
 	pub fn basic_hit(&self, ray: Ray) -> bool {
 		let inv = ray.direction().reciprocal();
 
